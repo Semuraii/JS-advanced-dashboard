be4358bc33dc33b1cfec2bf5b027aa47cd8eb48b
Semuraii <208602227+Semuraii@users.noreply.github.com>
2026-06-16 12:28:12 +0200

    input title, author and genre added

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 let books = json.parse(localStorage.getItem("books")) || [];
+
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const genreInput = document.querySelector("#genre");
