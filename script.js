eefb47739725e4d133c5da5856179d51281765ab
Semuraii <208602227+Semuraii@users.noreply.github.com>
2026-06-16 14:02:46 +0200

    function to save books added

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let books = json.parse(localStorage.getItem("books")) || [];
+let books = JSON.parse(localStorage.getItem("books")) || [];
 
 const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const genreInput = document.querySelector("#genre");
+
+function saveBooks() {
+    localStorage.setItem("books", JSON.stringify(books));
+}
