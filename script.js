88050f39c7cc0ff5bbba7f298dc11d260cfaedbd
Semuraii <208602227+Semuraii@users.noreply.github.com>
2026-06-23 12:07:11 +0200

    add book btn added

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const genreInput = document.querySelector("#genre");
 
+const addBookBtn = document.querySelector("#addBook");
+
+
 function saveBooks() {
     localStorage.setItem("books", JSON.stringify(books));
 }
